afa41e3399765599d06a1b99f3873fee04a2f059
Nathan <skeks463@gmail.com>
2020-04-24 20:56:36 +0900

    Calculator 객체 역할 분리 & 메소드 접근자 수정

--- src/main/java/CalculatorApplication.java
+++ src/main/java/CalculatorApplication.java
@@ -1,6 +1,19 @@
+import input.InputProcessor;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
 public class CalculatorApplication {
+
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.run();
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        InputProcessor processor = new InputProcessor(input);
+
+        List<Double> numbers = processor.getNumbers();
+        List<String> operators = processor.getOperators();
+        System.out.println(calculator.calculate(numbers, operators));
     }
 }
